103dc5ba9b01c525f85292532690017e5875c4e1
kevinlee168 <lisan521cool@gmail.com>
2022-10-19 17:11:59 +0800

    Create Template to show joke. close #5

--- src/main/java/com/kevin/jokesapp/JokesAppApplication.java
+++ src/main/java/com/kevin/jokesapp/JokesAppApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public interface JokesAppApplication {
+public class JokesAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JokesAppApplication.class, args);
